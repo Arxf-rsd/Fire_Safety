2b43b0387dada7f9c23e58fcf87109b92d4187be
Arif <149742559+Arxf-rsd@users.noreply.github.com>
2025-10-10 00:23:23 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,11 +5,9 @@ import cv2
 import numpy as np
 from ultralytics.utils.plotting import colors
 from typing import Tuple
-
 import time
 
-
-
+# Initialize OpenVINO
 core = ov.Core()
 
 device = widgets.Dropdown(
@@ -18,312 +16,169 @@ device = widgets.Dropdown(
     description="Device:",
     disabled=False,
 )
-
 device
 
-
-
+# Load model
 model = core.read_model(model="models/best.xml")
-compiled_model = core.compile_model(model = model, device_name = device.value)
+compiled_model = core.compile_model(model=model, device_name=device.value)
 
 input_layer = compiled_model.input(0)
 output_layer = compiled_model.output(0)
 print("Input Layer shape:", input_layer.shape)
 print("Output Layer shape:", output_layer.shape)
 
-
-
-
-
-
+# Load labels
 with open('models/metadata.yaml') as info:
     info_dict = yaml.load(info, Loader=yaml.Loader)
-
 labels = info_dict['names']
 print(labels)
 
-
-
-
-
-def letterbox(
-    img: np.ndarray,
-    new_shape: Tuple[int, int] = (640, 640),
-    color: Tuple[int, int, int] = (114, 114, 114),
-    auto: bool = False,
-    scale_fill: bool = False,
-    scaleup: bool = False,
-    stride: int = 32,
-):
-    """
-    Resize image and padding for detection. Takes image as input,
-    resizes image to fit into new shape with saving original aspect ratio and pads it to meet stride-multiple constraints
-
-    Parameters:
-      img (np.ndarray): image for preprocessing
-      new_shape (Tuple(int, int)): image size after preprocessing in format [height, width]
-      color (Tuple(int, int, int)): color for filling padded area
-      auto (bool): use dynamic input size, only padding for stride constrins applied
-      scale_fill (bool): scale image to fill new_shape
-      scaleup (bool): allow scale image if it is lower then desired input size, can affect model accuracy
-      stride (int): input padding stride
-    Returns:
-      img (np.ndarray): image after preprocessing
-      ratio (Tuple(float, float)): hight and width scaling ratio
-      padding_size (Tuple(int, int)): height and width padding size
-
-
-    """
-    # Resize and pad image while meeting stride-multiple constraints
-    shape = img.shape[:2]  # current shape [height, width]
+# Letterbox function (resizes with padding)
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=False,
+              scale_fill=False, scaleup=False, stride=32):
+    shape = img.shape[:2]
     if isinstance(new_shape, int):
         new_shape = (new_shape, new_shape)
-
-    # Scale ratio (new / old)
     r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+    if not scaleup:
         r = min(r, 1.0)
-
-    # Compute padding
-    ratio = r, r  # width, height ratios
+    ratio = r, r
     new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-    if auto:  # minimum rectangle
-        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
-    elif scale_fill:  # stretch
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]
+    if auto:
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)
+    elif scale_fill:
         dw, dh = 0.0, 0.0
         new_unpad = (new_shape[1], new_shape[0])
-        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
-
-    dw /= 2  # divide padding into 2 sides
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]
+    dw /= 2
     dh /= 2
-
-    if shape[::-1] != new_unpad:  # resize
+    if shape[::-1] != new_unpad:
         img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
     return img, ratio, (dw, dh)
 
-
-
+# Preprocess function
 def prepare_data(image, input_layer):
     input_w, input_h = input_layer.shape[2], input_layer.shape[3]
-
-    input_image = letterbox(np.array(image))[0]
-    input_image = cv2.resize(input_image, (input_w, input_h))
+    input_image = letterbox(np.array(image), (input_w, input_h))[0]
     input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
-    input_image = input_image/255
+    input_image = input_image / 255
     input_image = input_image.transpose(2, 0, 1)
     input_image = np.expand_dims(input_image, 0)
-
     return input_image
 
-
+# Inference test
 image = cv2.imread("images/test.jpg")
+if image is None:
+    raise FileNotFoundError("Image not found at 'images/test.jpg'")
+
 input_image = prepare_data(image, input_layer)
-print(np.shape(input_image))
-
+print("Input shape:", input_image.shape)
 output = compiled_model([input_image])[output_layer]
-print(output)
+print("Output shape:", output.shape)
 
-
+# Postprocessing
 def evaluate(output, conf_threshold):
-
-    boxes = []
-    scores = []
-    label_key = []
+    boxes, scores, label_key = [], [], []
     label_index = 0
-
     for class_ in output[0][4:]:
         for index in range(len(class_)):
             confidence = class_[index]
-
             if confidence > conf_threshold:
-                xcen = output[0][0][index]
-                ycen = output[0][1][index]
-                w = output[0][2][index]
-                h =output[0][3][index]
-
-                xmin = int(xcen - (w/2))
-                xmax = int(xcen + (w/2))
-                ymin = int(ycen - (h/2))
-                ymax = int(ycen + (h/2))
-
-                box = (xmin, ymin, xmax, ymax)
-                boxes.append(box)
+                xcen, ycen, w, h = output[0][0][index], output[0][1][index], output[0][2][index], output[0][3][index]
+                xmin, xmax = int(xcen - (w/2)), int(xcen + (w/2))
+                ymin, ymax = int(ycen - (h/2)), int(ycen + (h/2))
+                boxes.append((xmin, ymin, xmax, ymax))
                 scores.append(confidence)
                 label_key.append(label_index)
         label_index += 1
+    return np.array(boxes), np.array(scores), label_key
 
-    boxes = np.array(boxes)
-    scores = np.array(scores)
+# NMS and IOU
+def compute_iou(box, boxes, box_area, boxes_area):
+    ys1 = np.maximum(box[0], boxes[:, 0])
+    xs1 = np.maximum(box[1], boxes[:, 1])
+    ys2 = np.minimum(box[2], boxes[:, 2])
+    xs2 = np.minimum(box[3], boxes[:, 3])
+    intersections = np.maximum(ys2 - ys1, 0) * np.maximum(xs2 - xs1, 0)
+    unions = box_area + boxes_area - intersections
+    return intersections / unions
 
-    return boxes, scores, label_key
-
-
-conf_threshold =.1
-boxes, scores, label_key = evaluate(output, conf_threshold)
-
-print(boxes)
-print(scores)
-print(label_key)
-
-
-
-
-def non_max_suppression(boxes, scores, conf_threshold):	
-    assert boxes.shape[0] == scores.shape[0]
-    # bottom-left origin
-    ys1 = boxes[:, 0]
-    xs1 = boxes[:, 1]
-    # top-right target
-    ys2 = boxes[:, 2]
-    xs2 = boxes[:, 3]
-    # box coordinate ranges are inclusive-inclusive
+def non_max_suppression(boxes, scores, conf_threshold):
+    ys1, xs1 = boxes[:, 0], boxes[:, 1]
+    ys2, xs2 = boxes[:, 2], boxes[:, 3]
     areas = (ys2 - ys1) * (xs2 - xs1)
     scores_indexes = scores.argsort().tolist()
-    boxes_keep_index = []
-    while len(scores_indexes):
+    keep = []
+    while scores_indexes:
         index = scores_indexes.pop()
-        boxes_keep_index.append(index)
-        if not len(scores_indexes):
+        keep.append(index)
+        if not scores_indexes:
             break
-        ious = compute_iou(boxes[index], boxes[scores_indexes], areas[index],
-                           areas[scores_indexes])
-        filtered_indexes = set((ious > conf_threshold).nonzero()[0])
-        # if there are no more scores_index
-        # then we should pop it
-        scores_indexes = [
-            v for (i, v) in enumerate(scores_indexes)
-            if i not in filtered_indexes
-        ]
-    return np.array(boxes_keep_index)
+        ious = compute_iou(boxes[index], boxes[scores_indexes], areas[index], areas[scores_indexes])
+        scores_indexes = [v for i, v in enumerate(scores_indexes) if ious[i] <= conf_threshold]
+    return np.array(keep)
 
-
-
-def compute_iou(box, boxes, box_area, boxes_area):
-    # this is the iou of the box against all other boxes
-    assert boxes.shape[0] == boxes_area.shape[0]
-    # get all the origin-ys
-    # push up all the lower origin-xs, while keeping the higher origin-xs
-    ys1 = np.maximum(box[0], boxes[:, 0])
-    # get all the origin-xs
-    # push right all the lower origin-xs, while keeping higher origin-xs
-    xs1 = np.maximum(box[1], boxes[:, 1])
-    # get all the target-ys
-    # pull down all the higher target-ys, while keeping lower origin-ys
-    ys2 = np.minimum(box[2], boxes[:, 2])
-    # get all the target-xs
-    # pull left all the higher target-xs, while keeping lower target-xs
-    xs2 = np.minimum(box[3], boxes[:, 3])
-    # each intersection area is calculated by the
-    # pulled target-x minus the pushed origin-x
-    # multiplying
-    # pulled target-y minus the pushed origin-y
-    # we ignore areas where the intersection side would be negative
-    # this is done by using maxing the side length by 0
-    intersections = np.maximum(ys2 - ys1, 0) * np.maximum(xs2 - xs1, 0)
-    # each union is then the box area
-    # added to each other box area minusing their intersection calculated above
-    unions = box_area + boxes_area - intersections
-    # element wise division
-    # if the intersection is 0, then their ratio is 0
-    ious = intersections / unions
-    return ious
-
-
-
-if len(boxes):
-    nms_output = non_max_suppression(boxes, scores, conf_threshold)
-
-print(nms_output)
-
-
-def visualize(image, nms_output, boxes, label_key, scores, conf_threshold):
-    image_h, image_w, c = image.shape
+# Draw results
+def visualize(image, nms_output, boxes, label_key, scores):
+    image_h, image_w = image.shape[:2]
     input_w, input_h = input_layer.shape[2], input_layer.shape[3]
-
     for i in nms_output:
         xmin, ymin, xmax, ymax = boxes[i]
-        print(xmin, ymin, xmax, ymax)
-        
-        xmin = int(xmin*image_w/input_w)
-        xmax = int(xmax*image_w/input_w)
-        ymin = int(ymin*image_h/input_h)
-        ymax = int(ymax*image_h/input_h)
-
+        xmin = int(xmin * image_w / input_w)
+        xmax = int(xmax * image_w / input_w)
+        ymin = int(ymin * image_h / input_h)
+        ymax = int(ymax * image_h / input_h)
         label = label_key[i]
-        color = colors(label)
-        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 1)
-
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        text = str(int(scores[i]*100)) + "%" +labels[label]
-        font_scale = (image_w/1000)
-        label_width, label_height = cv2.getTextSize(text, font, font_scale, 1)[0]
-        cv2.rectangle(image, (xmin, ymin-label_height), (xmin + label_width, ymin), color, 1)
-        
-        cv2.putText(image, text, (xmin, ymin), font, font_scale, (255,255,255), 1, cv2.LINE_AA)
+        color = colors(label, True)
+        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
+        text = f"{labels[label]} {scores[i]*100:.1f}%"
+        cv2.putText(image, text, (xmin, ymin - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 2)
     return image
 
+# Predict single image
 def predict_image(image, conf_threshold):
     input_image = prepare_data(image, input_layer)
-
     start = time.time()
     output = compiled_model([input_image])[output_layer]
     end = time.time()
-
-    inference_time = end - start
-    
-    
     boxes, scores, label_key = evaluate(output, conf_threshold)
-
-    
     if len(boxes):
         nms_output = non_max_suppression(boxes, scores, conf_threshold)
-        visualized_image = visualize(image, nms_output, boxes, label_key, scores, conf_threshold)
+        image = visualize(image, nms_output, boxes, label_key, scores)
+    return image, end - start
 
-        return visualized_image, inference_time
-    else:
-        return image, inference_time
-
-
-mage = cv2.imread("images/test.jpg")
-conf_threshold = .15
-output_image,inference_time = predict_image(image, conf_threshold)
-
-print(inference_time)
-cv2.imshow("Image", image)
+# Test on single image
+conf_threshold = 0.15
+output_image, inference_time = predict_image(image, conf_threshold)
+print(f"Inference time: {inference_time:.3f}s")
+cv2.imshow("Detection Result", output_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# Real-time webcam inference
 def Main():
-    camera = cv2.VideoCapture(source)
-    inference_average =[]
-    #bg = cv2.imread(background)
-    
+    camera = cv2.VideoCapture(0)
+    if not camera.isOpened():
+        print("Error: Unable to open webcam.")
+        return
+    inference_times = []
     while True:
         ret, frame = camera.read()
-
         if not ret:
             break
-
         visualized_frame, inference_time = predict_image(frame, conf_threshold)
-        inference_average.append(inference_time)
-        #deployment = AddBackground(frame, bg)
-
-        
-        #cv2.imshow('Deployed Model', deployment)
-        cv2.imshow('Press Spacebar to Exit', visualized_frame)
+        inference_times.append(inference_time)
+        cv2.imshow("Press Spacebar to Exit", visualized_frame)
         if cv2.waitKey(1) & 0xFF == ord(' '):
             break
-    inf_ms = np.average(inference_average)*1000
-    print("The average inference time was " + str(np.round(inf_ms,2))+"milliseconds.")
+    avg_inf = np.mean(inference_times) * 1000
+    print(f"Average inference time: {avg_inf:.2f} ms")
     camera.release()
     cv2.destroyAllWindows()
 
-source = 0 #"videos/test.mp4"
-#background = "background.jpg"
-conf_threshold = .4
-if __name__ == '__main__':
+if __name__ == "__main__":
     Main()
