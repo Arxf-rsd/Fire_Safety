246bfac3f686c779614a7f86683f207621b97f9f
Arif <149742559+Arxf-rsd@users.noreply.github.com>
2025-10-10 09:34:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -51,10 +51,11 @@ def play_video(video_source):
 
 def play_live_camera():
     image = camera_input_live()
-    uploaded_image = PIL.Image.open(image)
-    uploaded_image_cv = cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
-    visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold)
-    st.image(visualized_image, channels = "BGR")
+    if image is not None:
+        uploaded_image = PIL.Image.open(image)
+        uploaded_image_cv = cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
+        visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold)
+        st.image(visualized_image, channels = "BGR")
 
 # ----------------------------
 # IMAGE INPUT
